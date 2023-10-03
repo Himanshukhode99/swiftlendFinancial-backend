566333804cec4288958f112a2678189f749582c3
Himanshukhode99 <himanshukhode001@.gmail.com>
2023-10-03 17:06:42 +0530

    Backend Project with only Packages

---
+++ Swift_Lend-Finacial/src/main/java/com/swiftLendFinancial/main/SwiftLendFinacialApplication.java
@@ -0,0 +1,13 @@
+package com.swiftLendFinancial.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftLendFinacialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftLendFinacialApplication.class, args);
+	}
+
+}
---
+++ Swift_Lend-Finacial/src/test/java/com/swiftLendFinancial/main/SwiftLendFinacialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftLendFinancial.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftLendFinacialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
