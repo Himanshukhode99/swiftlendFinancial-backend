7d65a82aec932c09ba64c9a8a4a5d474a4fa6894
PriyaBitake <priyaBitake21@gmail.com>
2023-10-11 16:57:43 +0530

    reject mail sendig task ready

--- Swift_Lend-Finacial/src/main/java/com/swiftLendFinancial/main/serviceImpl/Rejected_Service_Impl.java
+++ Swift_Lend-Finacial/src/main/java/com/swiftLendFinancial/main/serviceImpl/Rejected_Service_Impl.java
@@ -1,6 +1,9 @@
 package com.swiftLendFinancial.main.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.swiftLendFinancial.main.model.Rejected;
@@ -8,15 +11,32 @@ import com.swiftLendFinancial.main.repository.Rejected_Repository;
 import com.swiftLendFinancial.main.service.Rejected_Service;
 
 @Service
-public class Rejected_Service_Impl implements Rejected_Service {
+public class Rejected_Service_Impl implements Rejected_Service 
+{
+	@Value("${spring.mail.username}")
+	private String fromMail;
+	
+	@Autowired
+	JavaMailSender jms;
+
 	
 	@Autowired
 	Rejected_Repository rr;
 
 	@Override
-	public Rejected saveRejected(Rejected r) {
+	public Rejected saveRejected(Rejected r) 
+	{
+		Rejected reject=rr.save(r);
 		
-		return rr.save(r);
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setFrom(fromMail);
+		sm.setTo(reject.getEmail());
+		sm.setSubject("Reject loan application");
+		sm.setText("Your Loan application was Rejected Because \n Cibil Scor:-"+reject.getCibilscore()+"\n CibilStatus:-"+reject.getCibilstatus());
+		
+		jms.send(sm);
+		
+		return reject;
 	}
 
 }
